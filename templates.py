a80958dfb2b9d0988592618da3d875e92f191a10
niks-v <51901822+niks-v@users.noreply.github.com>
2020-03-24 15:22:30 +1000

    Update templates.py
    fixed template

--- templates.py
+++ templates.py
@@ -1,21 +1,20 @@
-# WEBSITE-COM = [url format, url min page, url max page, url extensions, parent class, price, address, date, rooms, bathrooms, cars, type]
-# USE WEBSITE DOMAIN WITH . REPLACED WITH -
+# [url format, url min page, url max page, url extensions, parent class, price, address, date, rooms, bathrooms, cars, type]
 
 from datetime import datetime
 
-websites = {
-    "realestate-com-au" : [
-        "https://www.realestate.com.au/buy/list-1",
+websites = [
+    [ # realestate.com.au # robots.txt disallows data collecting :( should have checked that first
+        "https://www.realestate.com.au/buy/list-{{number}}",
         1,
-        80,
+        3,
         "?activeSort=list-date",
-        ('article', {"class":'residential-card'}),
-        ('span', {"class":"property-price"}),
-        ('a', {"class":"residential-card__details-link"}),
-        str(datetime.now()),
-        ('span', {"class":"general-features__beds"}),
-        ('span', {"class":"general-features__baths"}),
-        ('span', {"class":"general-features__cars"}),
-        ('span', {"class":"residential-card__property-type"})
-        ]
-}
+        ('article', {"class":'residential-card'}), # parent 4
+        ('span', {"class":"property-price"}), # price 5 
+        ('a', {"class":"residential-card__details-link"}), # address 6
+        str(datetime.now()), # date 7
+        ('span', {"class":"general-features__beds"}), # beds 8
+        ('span', {"class":"general-features__baths"}), # baths 9
+        ('span', {"class":"general-features__cars"}), # cars 10
+        ('span', {"class":"residential-card__property-type"}) # type 11
+    ]
+]
